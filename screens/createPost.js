27aa182568c2fc0477fb36f0b952d38776326918
plaksha16 <88240876+plaksha16@users.noreply.github.com>
2022-06-25 19:43:41 +0530

    Add files via upload

---
+++ screens/createPost.js
@@ -0,0 +1,16 @@
+import  React,{Component} from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+
+
+
+export default class CreateStory extends Component{
+render(){
+  return(
+    <View>
+    <Text>Create Story</Text>
+    </View>
+  )
+}
+
+}
